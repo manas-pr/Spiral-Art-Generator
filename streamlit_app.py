d04b5d9a6428a92f6e117abe5549e512a93974ef
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-03-06 14:07:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# 🎨 Spiral Art Generator – Create Stunning Mathematical Spirals!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
